44d8a5eda3b957b709b931360c4100d5cabe4a7c
Vanessa <dayan.basulto@reduc.edu.cu>
2023-02-22 20:36:54 -0500

    conexion con la bd

---
+++ config/mongo.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+
+const dbConnect = () => {
+    const DB_URI = process.env.DB_URI;
+    mongoose.connect(DB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+        (err, res) => {
+            if (!err) {
+                console.log('***Conected***')
+            } else {
+                console.log('***Error Conected***')
+            }
+        });
+};
+module.exports = dbConnect
