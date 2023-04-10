454f7891a2547853e2065e9e23d2b00cdf5d6d73
Vanessa <dayan.basulto@reduc.edu.cu>
2023-04-10 17:31:18 -0400

    manejo de almacenamiento last test funcionamiento

--- routes/tracks.js
+++ routes/tracks.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const { getItem, createItem } = require('../controllers/tracks')
 const router = express.Router()
-const { getItems } = require('../controllers/tracks')
+const { validatorCreateItem } = require('../validators/tracks')
+const { getItems, getItem, createItem } = require('../controllers/tracks')
+
 
 router.get('/', getItems)
-router.post('/', createItem)
+router.post('/', validatorCreateItem, createItem)
 
 module.exports = router
