822874730d799eb7be71216814d49f95ee20d6c0
Vanessa <dayan.basulto@reduc.edu.cu>
2023-04-21 18:11:37 -0400

    proteccion a rutas usando jwt

--- routes/tracks.js
+++ routes/tracks.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const { validatorCreateItem, validatorGetItem } = require('../validators/tracks')
-const customHeader = require('../middleware/customHeader')
+const authMiddleware = require('../middleware/session')
 const { getItems, getItem, createItem, updateItem, deleteItem } = require('../controllers/tracks')
 
 
-router.get('/', getItems)
+router.get('/',authMiddleware, getItems)
 router.get('/:id',validatorGetItem, getItem)
 router.post('/', validatorCreateItem, createItem) 
 router.put('/:id', validatorGetItem, validatorCreateItem, updateItem) 
