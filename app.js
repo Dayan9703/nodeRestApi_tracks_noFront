18b4db8caf5f10f6d8819345b390891c7ce0c233
Vanessa <dayan.basulto@reduc.edu.cu>
2023-05-05 16:29:58 -0400

    manejador de errores abstraido usando Slack

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 require('dotenv').config()
-const express =  require('express')
+const express = require('express')
 const cors = require('cors')
+const morganBady = require('morgan-body')
+const loggerStream  = require('./utils/handleLogger')
 const dbConnect = require('./config/mongo')
-const app =  express()
+const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(express.static('storage'))
+
+morganBady(app, {
+    noColors: true,
+    stream: loggerStream,
+    skip: function(req,res){
+        return res.statusCode < 399
+    }
+})
 
 const port = process.env.PORT || 3000
 
 app.use('/api', require('./routes'))
 
 
-app.listen(port, () =>{
-    console.log('Tu app esta lista por http://localhost:'+port)
+app.listen(port, () => {
+    console.log('Tu app esta lista por http://localhost:' + port)
 })
 
 dbConnect()
